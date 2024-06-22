1337e288c09a9dd6be2f5ab2d88c76a4359a0f1a
Sakshet <sakshet@outlook.com>
2024-06-22 23:44:46 +0100

    Add Text

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -1,19 +1,19 @@
 import { createStyleSheet, useStyleSheet } from '@Core/styles';
+import { Heading, Text } from '@Core/text';
 import React from 'react';
 
 const appStyleSheet = createStyleSheet('appStyles', ({ isRed }: { isRed: boolean; }) => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    color: isRed ? 'red' : 'black',
-    fontSize: '100px',
   },
 }));
 export const App = () => {
   const classes = useStyleSheet(appStyleSheet, { isRed: true });
   return (
     <div className={classes.container}>
-      Hello World!
+      <Text typography="body03">Hello Text</Text>
+      <Heading typography="heading03">Hello Text</Heading>
     </div>
   );
 }
