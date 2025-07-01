2d09e49003d79e39d8e7ca0a8f9abb33a0208e12
Sakshet <sakshet@outlook.com>
2025-07-01 21:04:07 +0100

    mortgage calcultor init

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,149 +1,199 @@
-import { useServicesData } from '@Context';
-import { colors, Icon, Link, Text } from '@Core';
-import {
-  dragOption,
-  formerOptions,
-  latterOptions,
-  HEADER_HEIGHT,
-  HEADER_PADDING,
-  MIN_SIZE_FOR_DESKTOP,
-  MIN_SIZE_FOR_SMALL_SCREEN,
-} from '@Utils/constants';
-import { Service } from '@Utils/types';
+import { colors } from '@Core/colors';
+import { Link } from '@Core/link';
+import { Heading, Text } from '@Core/text';
+import { createStyleSheet, useStyleSheet } from '@Core/theme';
+import React from 'react';
 
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  background-color: ${colors.black};
-  flex-shrink: 0;
-  z-index: 1000;
-  position: fixed;
-  width: 100%;
-  &:hover {
-    opacity: 100%;
+const headerStyleSheet = createStyleSheet('headerStyle', () => ({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: '40px',
+    padding: '5px 30px',
+    background: colors.blue010,
   }
-`;
-
-const Content = styled.div`
-  padding: 10px 20px;
-  width: 100%;
-  box-sizing: border-box;
-  @media (min-width: ${MIN_SIZE_FOR_DESKTOP}px) {
-    width: 70%;
-  }
-`;
-
-const Menu = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  color: ${colors.gray050};
-  height: ${HEADER_HEIGHT};
-`;
-
-const SubMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: ${HEADER_PADDING}px 0;
-  margin-top: 10px;
-  gap: 20px;
-  border-top: 2px solid ${colors.gray100};
-`;
-
-export const Header = ({
-  onSubMenuHide,
-  onSubMenuShow,
-}: {
-  onSubMenuHide: () => void;
-  onSubMenuShow: () => void;
-}) => {
-  const { options } = useServicesData();
-  const [subMenu, setSubMenu] = useState<Service[]>([]);
-  const [width, setWidth] = useState<number>(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (subMenu.length) onSubMenuShow();
-    else onSubMenuHide();
-  }, [subMenu]);
-
+}));
+export const Header = () => {
+  const classes = useStyleSheet(headerStyleSheet, null);
   return (
-    <Container onMouseLeave={() => setSubMenu([])}>
-      <Content>
-        <Menu>
-          {(width > MIN_SIZE_FOR_SMALL_SCREEN
-            ? options
-            : [...formerOptions, ...latterOptions, dragOption]
-          ).map((option, key) => (
-            <MenuItem
-              key={key}
-              onClick={() => setSubMenu([])}
-              onHover={() => setSubMenu(option.subServices || [])}
-              option={option}
-            />
-          ))}
-        </Menu>
-        {subMenu.length > 0 && (
-          <SubMenu>
-            {subMenu.map((option, key) => (
-              <MenuItem
-                key={key}
-                large
-                onClick={() => setSubMenu([])}
-                option={option}
-              />
-            ))}
-          </SubMenu>
-        )}
-      </Content>
-    </Container>
+    <div className={classes.container}>
+      <Link to='/'>
+        <Heading typography='heading06'>AHORA</Heading>      
+      </Link>
+      <Actions />
+    </div>
+  )
+};
+
+const actionStyleSheet = createStyleSheet('actionStyles', () => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '30px',
+  }
+}));
+const Actions = () => {
+  const classes = useStyleSheet(actionStyleSheet, null);
+  return (
+    <div className={classes.container}>
+      <Link to='/mortgage-calculator'>
+        <Text typography='body04'>Mortgage Calculator</Text>
+      </Link>
+      <Link to='/about'>
+        <Text typography='body04'>About</Text>
+      </Link>
+    </div>
   );
 };
 
-const MenuItemWrapper = styled.div`
-  color: ${colors.white};
-  opacity: 60%;
-  &:hover {
-    opacity: 80%;
-  }
-`;
 
-const MenuItem = ({
-  large = false,
-  onClick,
-  onHover,
-  option,
-}: {
-  large?: boolean;
-  onClick: () => void;
-  onHover?: () => void;
-  option: Service;
-}) => {
-  return (
-    <MenuItemWrapper onMouseEnter={onHover}>
-      <Text typography={large ? 'body01' : 'body07'}>
-        <Link to={option.path || ''} onClick={onClick}>
-          {option.icon && (
-            <Icon name={option.icon} type={option.iconType || 'outlined'} />
-          )}
-          {option.label}
-        </Link>
-      </Text>
-    </MenuItemWrapper>
-  );
-};
+// import { useServicesData } from '@Context';
+// import { colors, Icon, Link, Text } from '@Core';
+// import {
+//   dragOption,
+//   formerOptions,
+//   latterOptions,
+//   HEADER_HEIGHT,
+//   HEADER_PADDING,
+//   MIN_SIZE_FOR_DESKTOP,
+//   MIN_SIZE_FOR_SMALL_SCREEN,
+// } from '@Utils/constants';
+// import { Service } from '@Utils/types';
+
+// import React, { useEffect, useState } from 'react';
+// import styled from 'styled-components';
+
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: center;
+//   background-color: ${colors.black};
+//   flex-shrink: 0;
+//   z-index: 1000;
+//   position: fixed;
+//   width: 100%;
+//   &:hover {
+//     opacity: 100%;
+//   }
+// `;
+
+// const Content = styled.div`
+//   padding: 10px 20px;
+//   width: 100%;
+//   box-sizing: border-box;
+//   @media (min-width: ${MIN_SIZE_FOR_DESKTOP}px) {
+//     width: 70%;
+//   }
+// `;
+
+// const Menu = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   align-items: center;
+//   width: 100%;
+//   color: ${colors.gray050};
+//   height: ${HEADER_HEIGHT};
+// `;
+
+// const SubMenu = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   justify-content: center;
+//   padding: ${HEADER_PADDING}px 0;
+//   margin-top: 10px;
+//   gap: 20px;
+//   border-top: 2px solid ${colors.gray100};
+// `;
+
+// export const Header = ({
+//   onSubMenuHide,
+//   onSubMenuShow,
+// }: {
+//   onSubMenuHide: () => void;
+//   onSubMenuShow: () => void;
+// }) => {
+//   const { options } = useServicesData();
+//   const [subMenu, setSubMenu] = useState<Service[]>([]);
+//   const [width, setWidth] = useState<number>(window.innerWidth);
+
+//   useEffect(() => {
+//     const handleResize = () => setWidth(window.innerWidth);
+//     window.addEventListener('resize', handleResize);
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     if (subMenu.length) onSubMenuShow();
+//     else onSubMenuHide();
+//   }, [subMenu]);
+
+//   return (
+//     <Container onMouseLeave={() => setSubMenu([])}>
+//       <Content>
+//         <Menu>
+//           {(width > MIN_SIZE_FOR_SMALL_SCREEN
+//             ? options
+//             : [...formerOptions, ...latterOptions, dragOption]
+//           ).map((option, key) => (
+//             <MenuItem
+//               key={key}
+//               onClick={() => setSubMenu([])}
+//               onHover={() => setSubMenu(option.subServices || [])}
+//               option={option}
+//             />
+//           ))}
+//         </Menu>
+//         {subMenu.length > 0 && (
+//           <SubMenu>
+//             {subMenu.map((option, key) => (
+//               <MenuItem
+//                 key={key}
+//                 large
+//                 onClick={() => setSubMenu([])}
+//                 option={option}
+//               />
+//             ))}
+//           </SubMenu>
+//         )}
+//       </Content>
+//     </Container>
+//   );
+// };
+
+// const MenuItemWrapper = styled.div`
+//   color: ${colors.white};
+//   opacity: 60%;
+//   &:hover {
+//     opacity: 80%;
+//   }
+// `;
+
+// const MenuItem = ({
+//   large = false,
+//   onClick,
+//   onHover,
+//   option,
+// }: {
+//   large?: boolean;
+//   onClick: () => void;
+//   onHover?: () => void;
+//   option: Service;
+// }) => {
+//   return (
+//     <MenuItemWrapper onMouseEnter={onHover}>
+//       <Text typography={large ? 'body01' : 'body07'}>
+//         <Link to={option.path || ''} onClick={onClick}>
+//           {option.icon && (
+//             <Icon name={option.icon} type={option.iconType || 'outlined'} />
+//           )}
+//           {option.label}
+//         </Link>
+//       </Text>
+//     </MenuItemWrapper>
+//   );
+// };
--- src/views/app/app.tsx
+++ src/views/app/app.tsx
@@ -1,158 +1,164 @@
-import { Header } from '@Components';
+// import { Header } from '@Components';
+
+import { Header } from '@Components/header';
 import { AppStateProvider, ServerStateProvider } from '@Context';
-import { useServicesData } from '@Context/data-processors';
-import { colors } from '@Core/colors';
-import { HEADER_HEIGHT, HEADER_PADDING } from '@Utils/constants';
-import { Service } from '@Utils/types';
-import { Content, MortgageCalculator } from '@Views';
+// import { useServicesData } from '@Context/data-processors';
 
-import React, { ReactElement, useEffect, useState } from 'react';
-import {
-  useLocation,
-  useNavigate,
-  Navigate,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import styled from 'styled-components';
-
-import { GlobalStyle } from '../../global-styles';
+// import { colors } from '@Core/colors';
 import { createStyleSheet, useStyleSheet } from '@Core/theme';
+import { MortgageCalculator } from '@Views';
 
-export const App = () => {
+// import { HEADER_HEIGHT, HEADER_PADDING } from '@Utils/constants';
+// import { Service } from '@Utils/types';
+// import { Content, MortgageCalculator } from '@Views';
+
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export const AppContainer = () => {
   return (
     <AppStateProvider useMockData={true}>
       <ServerStateProvider>
-        {/* <GlobalStyle />
-        <AppRoutes /> */}
-        <App2 />
+        {/* <GlobalStyle /> */}
+        {/* <AppRoutes /> */}
+        <App />
       </ServerStateProvider>
     </AppStateProvider>
   );
 };
 
-const appStyleSheet = createStyleSheet('appStyles', ({ color }: { color: string; }) => ({
+const appStyleSheet = createStyleSheet('appStyles', () => ({
   container: {
+    width: '100%',
+    height: '100%',
     display: 'flex',
-    color: color,
-    '&:hover': {
-      color: 'yellow'
-    }
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+  },
+  routes: {
+    padding: '15px 30px',
+    height: '100%'
   }
 }));
-const App2 = () => {
-
-  // contso
-  const classes = useStyleSheet(appStyleSheet, { color: 'red' });
+const App = () => {
+  const classes = useStyleSheet(appStyleSheet, null);
   return (
     <div className={classes.container}>
-      Hello
+      <Header />
+      <div className={classes.routes}>
+        <Routes>
+          <Route path="/" element={<div>Welcome</div>} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/mortgage-calculator" element={<MortgageCalculator />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </div>
   );
 };
 
-const generateRoutes = (
-  blurContent: boolean,
-  services: Service[],
-): JSX.Element[] => {
-  const routes: JSX.Element[] = [];
+// const generateRoutes = (
+//   blurContent: boolean,
+//   services: Service[],
+// ): JSX.Element[] => {
+//   const routes: JSX.Element[] = [];
 
-  const createRoutes = (services: Service[]) => {
-    services.forEach((service, key) => {
-      routes.push(
-        <Route
-          key={key}
-          path={service.path}
-          element={
-            <StyledComponent blurcontent={blurContent}>
-              <Content />
-            </StyledComponent>
-          }
-        />,
-      );
-      if (service.subServices && service.subServices.length > 0) {
-        createRoutes(service.subServices);
-      }
-    });
-  };
+//   const createRoutes = (services: Service[]) => {
+//     services.forEach((service, key) => {
+//       routes.push(
+//         <Route
+//           key={key}
+//           path={service.path}
+//           element={
+//             <StyledComponent blurcontent={blurContent}>
+//               <Content />
+//             </StyledComponent>
+//           }
+//         />,
+//       );
+//       if (service.subServices && service.subServices.length > 0) {
+//         createRoutes(service.subServices);
+//       }
+//     });
+//   };
 
-  createRoutes(services);
-  return routes;
-};
+//   createRoutes(services);
+//   return routes;
+// };
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-`;
-const StyledRoute = styled.div<{ $blurcontent: string }>`
-  flex-grow: 1;
-  overflow: auto;
-  background: ${colors.gray010};
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  margin-top: ${HEADER_HEIGHT + 2 * HEADER_PADDING}px;
-  opacity: ${(props) => (props.$blurcontent === 'true' ? '25%' : '100%')};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// const Wrapper = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   flex-wrap: nowrap;
+// `;
+// const StyledRoute = styled.div<{ $blurcontent: string }>`
+//   flex-grow: 1;
+//   overflow: auto;
+//   background: ${colors.gray010};
+//   position: absolute;
+//   height: 100%;
+//   width: 100%;
+//   margin-top: ${HEADER_HEIGHT + 2 * HEADER_PADDING}px;
+//   opacity: ${(props) => (props.$blurcontent === 'true' ? '25%' : '100%')};
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
-const StyledComponent = ({
-  blurcontent,
-  children,
-}: {
-  blurcontent: boolean;
-  children: ReactElement;
-}) => (
-  <StyledRoute $blurcontent={blurcontent.toString()}>{children}</StyledRoute>
-);
+// const StyledComponent = ({
+//   blurcontent,
+//   children,
+// }: {
+//   blurcontent: boolean;
+//   children: ReactElement;
+// }) => (
+//   <StyledRoute $blurcontent={blurcontent.toString()}>{children}</StyledRoute>
+// );
 
-export const AppRoutes = () => {
-  const [blurContent, setBlurContent] = useState<boolean>(false);
-  const { options } = useServicesData();
+// export const AppRoutes = () => {
+//   const [blurContent, setBlurContent] = useState<boolean>(false);
+//   const { options } = useServicesData();
 
-  const navigate = useNavigate();
-  const location = useLocation();
+//   const navigate = useNavigate();
+//   const location = useLocation();
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const redirectPath = params.get('redirect');
-    options;
-    if (redirectPath) {
-      navigate(redirectPath);
-    }
-  }, [navigate, location.search]);
+//   useEffect(() => {
+//     const params = new URLSearchParams(location.search);
+//     const redirectPath = params.get('redirect');
+//     options;
+//     if (redirectPath) {
+//       navigate(redirectPath);
+//     }
+//   }, [navigate, location.search]);
 
-  return (
-    <Wrapper>
-      <Header
-        onSubMenuHide={() => setBlurContent(false)}
-        onSubMenuShow={() => setBlurContent(true)}
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <StyledComponent blurcontent={blurContent}>
-              <Content />
-            </StyledComponent>
-          }
-        />
-        <Route
-          path="/mortgage-calculator"
-          element={
-            <StyledComponent blurcontent={blurContent}>
-              <MortgageCalculator />
-            </StyledComponent>
-          }
-        />
-        {generateRoutes(blurContent, options)}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Wrapper>
-  );
-};
+//   return (
+//     <Wrapper>
+//       <Header
+//         onSubMenuHide={() => setBlurContent(false)}
+//         onSubMenuShow={() => setBlurContent(true)}
+//       />
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             <StyledComponent blurcontent={blurContent}>
+//               <Content />
+//             </StyledComponent>
+//           }
+//         />
+//         <Route
+//           path="/mortgage-calculator"
+//           element={
+//             <StyledComponent blurcontent={blurContent}>
+//               <MortgageCalculator />
+//             </StyledComponent>
+//           }
+//         />
+//         {generateRoutes(blurContent, options)}
+//         <Route path="*" element={<Navigate to="/" />} />
+//       </Routes>
+//     </Wrapper>
+//   );
+// };
