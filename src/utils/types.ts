5144dfc5110a1f541790baa4920a9207003e06ee
Sakshet <sakshet@outlook.com>
2025-07-22 20:15:21 +0100

    Combine Calculators

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,18 @@
+export type CompoundInput = {
+  principal: number;
+  rate: number;
+  timesCompounded: number;
+  years: number;
+  monthlyAddition: number;
+};
+
+export type MortgageInput = {
+  deposit: number;
+  interest: number;
+  price: number;
+  years: number;
+};
+
 export type Service = {
   hoverDisabled?: boolean;
   icon?: string;
@@ -7,11 +22,4 @@ export type Service = {
   onClick?: () => void;
   path?: string;
   subServices?: Service[];
-};
-
-export type MortgageInput = {
-  deposit: number;
-  interest: number;
-  price: number;
-  years: number;
 };
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,5 @@
 export * from './about';
 export * from './app';
+export * from './compound-interest';
 export * from './homepage';
 export * from './mortgage-calculator';
